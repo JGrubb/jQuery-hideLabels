102389fc011013f7913fbe5bd59c08a230178e49
JGrubb <therealjohnnygrubb@gmail.com>
2012-01-09 16:36:50 -0500

    removing coffee, making nicer

--- jquery.hideLabels.js
+++ jquery.hideLabels.js
@@ -1,6 +1,6 @@
-(function() {
-  jQuery.fn.hideLabels = function() {
-    return this.each(function() {
+(function($) {
+  $.fn.hideLabels = function() {
+    this.each(function() {
       $(this).siblings("input").focus(function() {
         return $(this).prev().hide();
       });
@@ -9,11 +9,11 @@
           return $(this).prev().show();
         }
       });
-      return $(this).siblings("input").each(function() {
+      $(this).siblings("input").each(function() {
         if (this.value) {
           return $(this).prev().hide();
         }
       });
     });
   };
-}).call(this);
+})(jQuery);
