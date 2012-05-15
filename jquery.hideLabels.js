9ad203bb1109be6562250f49eb041496b9f0153a
John Grubb <therealjohnnygrubb@gmail.com>
2012-05-15 10:07:28 -0400

    rewrite and example page

--- jquery.hideLabels.js
+++ jquery.hideLabels.js
@@ -8,19 +8,6 @@
           $(this).prev().show();
         }
       });
-//      $(this).siblings("input").focus(function() {
-//        return $(this).prev().hide();
-//      });
-//      $(this).siblings("input").blur(function() {
-//        if (!this.value) {
-//          return $(this).prev().show();
-//        }
-//      });
-//      $(this).siblings("input").each(function() {
-//        if (this.value) {
-//          return $(this).prev().hide();
-//        }
-//      });
     });
   };
 })(jQuery);
