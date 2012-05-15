fe8f8f819617a80924d5ff5c4b03579a3cfe4c16
John Grubb <therealjohnnygrubb@gmail.com>
2012-05-15 07:26:38 -0400

    rewrite:

--- jquery.hideLabels.js
+++ jquery.hideLabels.js
@@ -1,19 +1,26 @@
 (function($) {
   $.fn.hideLabels = function() {
     this.each(function() {
-      $(this).siblings("input").focus(function() {
-        return $(this).prev().hide();
-      });
-      $(this).siblings("input").blur(function() {
+      $(this).siblings('input').bind('focus', function() {
+        $(this).prev().hide();
+      }).bind('blur', function() {
         if (!this.value) {
-          return $(this).prev().show();
+          $(this).prev().show();
         }
       });
-      $(this).siblings("input").each(function() {
-        if (this.value) {
-          return $(this).prev().hide();
-        }
-      });
+//      $(this).siblings("input").focus(function() {
+//        return $(this).prev().hide();
+//      });
+//      $(this).siblings("input").blur(function() {
+//        if (!this.value) {
+//          return $(this).prev().show();
+//        }
+//      });
+//      $(this).siblings("input").each(function() {
+//        if (this.value) {
+//          return $(this).prev().hide();
+//        }
+//      });
     });
   };
 })(jQuery);
